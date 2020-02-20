6e24db75377704fb71bac7111d19748e1d26eb2d
Andy Fryman <49570822+andyfryman@users.noreply.github.com>
2020-02-21 00:46:31 +0300

    Add heal strategy

--- src/main/java/andyfryman/chinchopa/strategy/HealStrategy.java
+++ src/main/java/andyfryman/chinchopa/strategy/HealStrategy.java
@@ -6,33 +6,31 @@ import org.slf4j.LoggerFactory;
 import skadistats.clarity.model.CombatLogEntry;
 import skadistats.clarity.processor.gameevents.OnCombatLogEntry;
 
-public class DamageStrategy {
+public class HealStrategy {
 
-    private final Logger log = LoggerFactory.getLogger(DamageStrategy.class.getPackage().getClass());
+    private final Logger log = LoggerFactory.getLogger(HealStrategy.class.getPackage().getClass());
 
     private static final Compile compile = new Compile();
 
-    public DamageStrategy() {
+    public HealStrategy() {
         onStart();
     }
 
     public void onStart() {
-        log.info("time,attacker,damageSource,target,damageTarget,inflictor,value,from,to");
+        log.info("time,attacker,target,inflictor,value,from,to");
     }
 
     @OnCombatLogEntry
     public void onCombatLogEntry(CombatLogEntry cle) {
         switch (cle.getType()) {
-            case DOTA_COMBATLOG_DAMAGE:
-                log.info("{},{},{},{},{},{},{},{},{}",
+            case DOTA_COMBATLOG_HEAL:
+                log.info("{},{},{},{},{},{},{}",
                         compile.time(cle),
                         compile.attacker(cle),
-                        compile.damageSource(cle),
                         compile.target(cle),
-                        compile.damageTarget(cle),
                         compile.inflictor(cle),
                         cle.getValue(),
-                        cle.getHealth() + cle.getValue(),
+                        cle.getHealth() - cle.getValue(),
                         cle.getHealth()
                 );
                 break;
