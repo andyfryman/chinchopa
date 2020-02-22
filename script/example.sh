8de16cfed4ef486dd4bb8d08bfd3787babb64dc5
Andy Fryman <49570822+andyfryman@users.noreply.github.com>
2020-02-22 20:01:56 +0300

    Generate only specified example

--- script/example.sh
+++ script/example.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 REPLAY_PATH="/Users/gamer/Downloads"
 REPLAY_FILE="replay.dem"
-REPLAY_COMM=(item purchase damage death experience gold heal)
+REPLAY_COMM=()
+if [ "$#" -eq  "0" ]
+  then
+    REPLAY_COMM=(ability item purchase damage death experience gold heal)
+  else
+    REPLAY_COMM=($1)
+fi
 for i in "${REPLAY_COMM[@]}"
 do
-  echo "[EXAMPLE] " $i
+  echo "[EXAMPLE]" $i
   sh ./script/run.sh $REPLAY_PATH $REPLAY_FILE $i > ./example/$i.csv
 done
